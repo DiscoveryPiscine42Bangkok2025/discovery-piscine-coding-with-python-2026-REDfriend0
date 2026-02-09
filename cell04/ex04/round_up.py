20e98e7e8d7da28d4e3b64e5a1ede268a9447703
1REDfriend <ttk666.th@gmail.com>
2026-02-09 22:08:15 +0700

    Create round_up.py

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,18 @@
+import math
+
+def main():
+    user_input = input("Give me a number : ")
+    try:
+        number = float(user_input)
+
+        rounded_up = math.ceil(number)
+        print(rounded_up)
+        if number.is_integer():
+            print("This number is an integer.")
+        else:
+            print("This number is a decimal.")
+    except ValueError:
+        print("That is not a valid number.")
+
+if __name__ == "__main__":
+    main()
