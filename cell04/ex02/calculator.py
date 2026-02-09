60c14025e755ac72d52ff6b448136722160cf512
1REDfriend <ttk666.th@gmail.com>
2026-02-09 22:08:12 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,12 @@
+def main():
+    first_number = float(input("Give me the first number : "))
+    second_number = float(input("Give me to the second number : "))
+    print("Thank You!")
+
+    print(f"{first_number} + {second_number} = {first_number + second_number}")
+    print(f"{first_number} - {second_number} = {first_number - second_number}")
+    print(f"{first_number} / {second_number} = {first_number / second_number}")
+    print(f"{first_number} * {second_number} = {first_number * second_number}")
+
+if __name__ == "__main__":
+    main()
