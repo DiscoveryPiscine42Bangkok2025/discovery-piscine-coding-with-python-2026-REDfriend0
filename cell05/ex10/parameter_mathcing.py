364b705fa1ad81549931499144f1aeef50c830d0
1REDfriend <ttk666.th@gmail.com>
2026-02-10 07:46:43 +0700

    Create parameter_mathcing.py

---
+++ cell05/ex10/parameter_mathcing.py
@@ -0,0 +1,8 @@
+
+import sys 
+
+if len(sys.argv) < 2: print("none")
+else: 
+    word = str(input("what was the parameter? "))
+    if word == sys.argv[1]:print("Good job!")
+    else: print("Nope, sorry...")
