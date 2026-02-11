5e9e00367c1b4aadea2a9f38c276203913cf8fed
1REDfriend <ttk666.th@gmail.com>
2026-02-11 14:21:20 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -2,5 +2,7 @@
 import sys
 import re
 
-if len(sys.argv) < 3: print("none")
-else : print(len(re.findall(r'\bthe\b', sys.argv[2])))
+if len(sys.argv) < 3: 
+    print("none")
+else : 
+    print(len(re.findall(r'\bthe\b', sys.argv[2])))
