9bdc6d878cf6c9c7acff0bf4496d29d8c957b14c
1REDfriend <ttk666.th@gmail.com>
2026-02-11 16:02:40 +0700

    Update parameter_mathcing.py

--- cell05/ex10/parameter_mathcing.py
+++ cell05/ex10/parameter_mathcing.py
@@ -1,8 +1,11 @@
 
 import sys 
 
-if len(sys.argv) < 2: print("none")
+if len(sys.argv) < 2: 
+    print("none")
 else: 
     word = str(input("what was the parameter? "))
-    if word == sys.argv[1]:print("Good job!")
-    else: print("Nope, sorry...")
+    if word == sys.argv[1]: 
+        print("Good job!")
+    else: 
+        print("Nope, sorry...")
